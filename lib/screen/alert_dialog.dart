5aaf81a6e63759e23206f9d79d98416d470b4747
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-12 01:30:01 +0600

    first commit

---
+++ lib/screen/alert_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlertDialogBox extends StatelessWidget {
+  const AlertDialogBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("MyApp"),
+      ),
+    );
+  }
+}
---
+++ lib/screen/text_form.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextForm extends StatelessWidget {
+  const TextForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
