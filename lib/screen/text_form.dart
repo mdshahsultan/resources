1dbb83b34025c31da77a6edb720c56a595c4f25c
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-12 02:40:09 +0600

    update

--- lib/screen/text_form.dart
+++ lib/screen/text_form.dart
@@ -5,11 +5,38 @@ class TextForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Button Style Start
+      ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, 60)
+      );
+    //Button Style End
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text("Text Form Field", style: TextStyle(color: Colors.white),),
         centerTitle: true,
+      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(decoration: InputDecoration( border: OutlineInputBorder(), labelText: "First Name"),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(decoration: InputDecoration( border: OutlineInputBorder(), labelText: "Last Name"),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(decoration: InputDecoration( border: OutlineInputBorder(), labelText: "Email Address"),),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: ElevatedButton(onPressed: (){}, child: Text("Submit"), style: buttonStyle,),
+          ),
+        ],
       ),
     );
   }
