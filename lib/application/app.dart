bd46da4f4f910b6f48bf2300ec1ce3546ea9ce5c
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-12 03:41:39 +0600

    update

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../screen/alert_dialog.dart';
+import '../screen/list_view.dart';
 import '../screen/text_form.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TextForm(),
+      home: ListViewCreate(),
     );
   }
 }
